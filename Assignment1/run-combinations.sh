f19c70a29476770b4290c91f5807e417c7dc72e8
lukasbygmal <lukasbygmal@gmail.com>
2024-09-05 14:41:49 +0200

    Fixed issue in script

--- Assignment1/run-combinations.sh
+++ Assignment1/run-combinations.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-# Define the values for T and N
 T_values=(1 2 4 8 16 32)
 N_values=(1 2 4 8 16 32)
 
-# Loop over each combination of T and N
 for T in "${T_values[@]}"; do
     for N in "${N_values[@]}"; do
         echo "Running program with T=$T and N=$N"
-        ./your_program $T $N  # Replace "your_program" with the actual name of your executable
+        ./performance $T $N  
     done
 done
