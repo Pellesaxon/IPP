c62235d37f563f3fe47c6dc603313736ae218224
Oskar Perers <oskar.perers@telia.com>
2024-10-14 23:13:50 +0200

    testing private

--- Assignment3/gaus_row.cpp
+++ Assignment3/gaus_row.cpp
@@ -52,7 +52,7 @@ int main(int argc, char *argv[]) {
     for (int row = matrix_size-1; row >= 0; row--) {
         x[row] = b[row];
 
-        #pragma omp parallel for shared(A, b, x) private(val) reduction(-:sum) schedule(static)
+        #pragma omp parallel for private(val, A, b, x) reduction(-:sum) schedule(static)
         for (int col = row+1; col < matrix_size; col++) {
             val = A[row][col] * x[col];
             sum -= val;
