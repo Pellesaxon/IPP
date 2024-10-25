e39d15c25e809226472ebae303a773212a04fc0c
Oskar Perers <oskar.perers@telia.com>
2024-10-25 14:15:53 +0200

    try another allocation method

--- Assignment4/eratosthenes_mpi.cpp
+++ Assignment4/eratosthenes_mpi.cpp
@@ -85,7 +85,7 @@ int main(int argc, char *argv[]) {
     
 
     int MAX_MSG_SIZE = chunk_size+size;
-    bool msg[MAX_MSG_SIZE];
+    bool* msg = new bool[MAX_MSG_SIZE];
     if (rank){
         //save you primes to msg, send msg
         for (int msg_index = 0; msg_index < (end-start+1); msg_index++){
